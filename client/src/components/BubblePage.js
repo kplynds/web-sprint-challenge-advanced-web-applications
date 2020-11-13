c5b6f19154b279df48f2e91d81a2f3792fb63c02
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-11-13 10:46:25 -0800

    basic functionality done

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,17 +3,43 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../utils/axiosWAuth";
 
-const BubblePage = () => {
+const BubblePage = (props) => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  const getData = () => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then((res) => {
+        console.log(res);
+        setColorList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const signout = () => {
+    localStorage.removeItem("token");
+    props.history.push("/");
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
+    <div>
+      <div><button onClick={signout}>Signout</button></div>
+      <div>
+        <ColorList colors={colorList} updateColors={setColorList} />
+      </div>
+      <div>
+        <Bubbles colors={colorList} />
+      </div>
+    </div>
   );
 };
 
